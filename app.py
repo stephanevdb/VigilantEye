8a09f9d626e7730ea9bd8a573208e35580671ac8
stephanevdb <mail@stephanevdb.com>
2023-10-26 22:24:36 +0200

    add settings

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/settings")
+def settings():
+    return render_template('settings.html')
+
 @app.route("/test")
 def test():
     return render_template('test.html'), 200
